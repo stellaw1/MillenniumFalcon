6601f3eabbfffeebf594e436afebaad7a670e30b
Stella W <46392676+stellaw1@users.noreply.github.com>
2019-10-20 18:21:43 -0700

    init

--- src/main/java/cpen221/mp2/graph/Graph.java
+++ src/main/java/cpen221/mp2/graph/Graph.java
@@ -9,7 +9,213 @@ import java.util.*;
  */
 public class Graph<V extends Vertex, E extends Edge<V>> implements ImGraph<V, E>, IGraph<V, E> {
 
-    // TODO: Implement this type
+    //methods from IGraph
+    /**
+     * Add a vertex to the graph
+     *
+     * @param v vertex to add
+     * @return true if the vertex was added successfully and false otherwise
+     */
+    public boolean addVertex(V v) {
+
+    }
+
+    /**
+     * Check if a vertex is part of the graph
+     *
+     * @param v vertex to check in the graph
+     * @return true of v is part of the graph and false otherwise
+     */
+    public boolean vertex(V v) {
+
+    }
+
+    /**
+     * Add an edge of the graph
+     *
+     * @param e the edge to add to the graph
+     * @return true if the edge was successfully added and false otherwise
+     */
+    public boolean addEdge(E e) {
+
+    }
+
+    /**
+     * Check if an edge is part of the graph
+     *
+     * @param e the edge to check in the graph
+     * @return true if e is an edge in the graoh and false otherwise
+     */
+    public boolean edge(E e) {
+
+    }
+
+    /**
+     * Check if v1-v2 is an edge in the graph
+     *
+     * @param v1 the first vertex of the edge
+     * @param v2 the second vertex of the edge
+     * @return true of the v1-v2 edge is part of the graph and false otherwise
+     */
+
+    public boolean edge(V v1, V v2) {
+
+    }
+
+    /**
+     * Determine the length on an edge in the graph
+     *
+     * @param v1 the first vertex of the edge
+     * @param v2 the second vertex of the edge
+     * @return the length of the v1-v2 edge if this edge is part of the graph
+     */
+    public int edgeLength(V v1, V v2) {
+
+    }
+
+    /**
+     * Obtain the sum of the lengths of all edges in the graph
+     *
+     * @return the sum of the lengths of all edges in the graph
+     */
+    public int edgeLengthSum() {
+
+    }
+
+    /**
+     * Remove an edge from the graph
+     *
+     * @param e the edge to remove
+     * @return true if e was successfully removed and false otherwise
+     */
+    public boolean remove(E e) {
+
+    }
+
+    /**
+     * Remove a vertex from the graph
+     *
+     * @param v the vertex to remove
+     * @return true if v was successfully removed and false otherwise
+     */
+    public boolean remove(V v) {
+
+    }
+
+
+    /**
+     * Obtain a set of all vertices in the graph.
+     * Access to this set **should not** permit graph mutations.
+     *
+     * @return a set of all vertices in the graph
+     */
+    public Set<V> allVertices() {
+
+    }
+
+    /**
+     * Obtain a set of all vertices incident on v.
+     * Access to this set **should not** permit graph mutations.
+     *
+     * @param v the vertex of interest
+     * @return all edges incident on v
+     */
+    public Set<E> allEdges(V v) {
+
+    }
+
+    /**
+     * Obtain a set of all edges in the graph.
+     * Access to this set **should not** permit graph mutations.
+     *
+     * @return all edges in the graph
+     */
+    public Set<E> allEdges() {
+
+    }
+
+    /**
+     * Obtain all the neighbours of vertex v.
+     * Access to this map **should not** permit graph mutations.
+     *
+     * @param v is the vertex whose neighbourhood we want.
+     * @return a map containing each vertex w that neighbors v and the edge between v and w.
+     */
+    Map<V, E> getNeighbours(V v) {
+
+    }
+
+
+    //ImGraph
+    /**
+     * Compute the shortest path from source to sink
+     *
+     * @param source the start vertex
+     * @param sink   the end vertex
+     * @return the vertices, in order, on the shortest path from source to sink (both end points are part of the list)
+     */
+    List<V> shortestPath(V source, V sink) {
+
+    }
+
+    /**
+     * Compute the minimum spanning tree of the graph.
+     * See https://en.wikipedia.org/wiki/Minimum_spanning_tree
+     *
+     * @return a list of edges that forms a minimum spanning tree of the graph
+     */
+    List<E> minimumSpanningTree() {
+
+    }
+
+    /**
+     * Compute the length of a given path
+     *
+     * @param path indicates the vertices on the given path
+     * @return the length of path
+     */
+    int pathLength(List<V> path) {
+
+    }
+
+    /**
+     * Obtain all vertices w that are no more than a <em>path distance</em> of range from v.
+     *
+     * @param v     the vertex to start the search from.
+     * @param range the radius of the search.
+     * @return a set of vertices that are within range of v (this set does not contain v).
+     */
+    Set<V> search(V v, int range) {
+
+    }
+
+    /**
+     * Compute the diameter of the graph.
+     * <ul>
+     * <li>The diameter of a graph is the length of the longest shortest path in the graph.</li>
+     * <li>If a graph has multiple components then we will define the diameter
+     * as the diameter of the largest component.</li>
+     * </ul>
+     *
+     * @return the diameter of the graph.
+     */
+    int diameter() {
+
+    }
+
+    /**
+     * Find the edge that connects two vertices if such an edge exists.
+     * This method should not permit graph mutations.
+     *
+     * @param v1 one end of the edge
+     * @param v2 the other end of the edge
+     * @return the edge connecting v1 and v2
+     */
+    public E getEdge(V v1, V v2){
+
+    }
+
+
 
     //// add all new code above this line ////
 
