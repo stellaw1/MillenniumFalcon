035b6fc2fe1a4048eafbae29000ae3cd3a81ffe2
David Wang <davidw0311@gmail.com>
2019-10-31 20:52:10 -0700

    implemented gather stage

--- src/main/java/cpen221/mp2/spaceship/MillenniumFalcon.java
+++ src/main/java/cpen221/mp2/spaceship/MillenniumFalcon.java
@@ -9,10 +9,7 @@ import cpen221.mp2.models.Planet;
 import cpen221.mp2.models.PlanetStatus;
 import cpen221.mp2.util.Heap;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 /**
  * An instance implements the methods needed to complete the mission.
@@ -22,9 +19,38 @@ public class MillenniumFalcon implements Spaceship {
 
     @Override
     public void hunt(HunterStage state) {
+        HashMap<Integer, Integer> stagesMap = new HashMap<>();
+        HashSet<Integer> visitedPlanetIDs = new HashSet<>();
 
-        // TODO: Implement this method
+        stagesMap.put(state.currentID(), state.currentID());
+        visitedPlanetIDs.add(state.currentID());
+
+        while(!state.onKamino()){
+            ArrayList<PlanetStatus> allNeighbors = new ArrayList<>();
+            HashSet<Integer> allNeighborsIDSet = new HashSet<>();
+
+            for (PlanetStatus thisNeighbor : state.neighbors()){
+                allNeighbors.add(thisNeighbor);
+                allNeighborsIDSet.add(thisNeighbor.id());
+            }
+            Collections.sort(allNeighbors);
+
+
+            for (int i = allNeighbors.size()-1; i > 0; i--){
+                int thisID = allNeighbors.get(i).id();
+                if (!visitedPlanetIDs.contains(thisID)){
+                    stagesMap.put(thisID, state.currentID());
+                    visitedPlanetIDs.add(thisID);
+                    state.moveTo(thisID);
+                    break;
+                }
+            }
+
+
+        }
+        return;
     }
+
 
     @Override
     public void gather(GathererStage state) {
