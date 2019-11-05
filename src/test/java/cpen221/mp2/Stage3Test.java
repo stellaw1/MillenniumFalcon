c371131fd758b58b7ecfc72795d92b960b9a4fdd
Stella W <46392676+stellaw1@users.noreply.github.com>
2019-11-05 13:41:14 -0800

    kamino test

--- src/test/java/cpen221/mp2/Stage3Test.java
+++ src/test/java/cpen221/mp2/Stage3Test.java
@@ -1,10 +1,6 @@
 package cpen221.mp2;
 
 import cpen221.mp2.controllers.Kamino;
-import cpen221.mp2.graph.Edge;
-import cpen221.mp2.graph.Graph;
-import cpen221.mp2.graph.Vertex;
-import cpen221.mp2.models.Universe;
 import cpen221.mp2.spaceship.MillenniumFalcon;
 import cpen221.mp2.views.BenchmarkView;
 import org.junit.Assert;
@@ -12,35 +8,78 @@ import org.junit.Test;
 
 import java.util.*;
 
+
 import static org.junit.Assert.*;
 
 public class Stage3Test {
 
-    //create a universe instance
-    Universe myUniverse;
-    Kamino myKamino;
-
     @Test
-    public void testHunt() {
-        String[] args = {"--benchmark"};
-        myKamino.main(args);
-
-        //view.scoreStats();
-    }
-
-    @Test
-    public void testGather() {
+    public void testMF() {
         long seed = 4646556996762522249L;
         BenchmarkView view = new BenchmarkView();
-        Kamino test = new Kamino(seed, new MillenniumFalcon(), view);
+        Kamino myKamino = new Kamino(seed, new MillenniumFalcon(), view);
 
-        String[] args = {"--benchmark"};
+        String[] args = {"--benchmark", Long.toString(seed)};
 
-        test.main(args);
+        myKamino.main(args);
+
+        assertTrue(myKamino.huntSucceeded());
+        assertTrue(myKamino.gatherSucceeded());
 
         view.scoreStats();
-
+        view.timeStats();
     }
 
+    @Test
+    public void testMF1() {
+        long seed = 5380112710085299282L;
+        BenchmarkView view = new BenchmarkView();
+        Kamino myKamino = new Kamino(seed, new MillenniumFalcon(), view);
+
+        String[] args = {"--benchmark", Long.toString(seed)};
+
+        myKamino.main(args);
+
+        assertTrue(myKamino.huntSucceeded());
+        assertTrue(myKamino.gatherSucceeded());
+
+        view.scoreStats();
+        view.timeStats();
+    }
+
+    @Test
+    public void testMF2() {
+        long seed = 8865034956231574663L;
+        BenchmarkView view = new BenchmarkView();
+        Kamino myKamino = new Kamino(seed, new MillenniumFalcon(), view);
+
+        String[] args = {"--benchmark", Long.toString(seed)};
+
+        myKamino.main(args);
+
+        assertTrue(myKamino.huntSucceeded());
+        assertTrue(myKamino.gatherSucceeded());
+
+        view.scoreStats();
+        view.timeStats();
+    }
+
+    //test on huge planet with 700+ planets
+    @Test
+    public void testMF3() {
+        long seed = -2912025469858177646L;
+        BenchmarkView view = new BenchmarkView();
+        Kamino myKamino = new Kamino(seed, new MillenniumFalcon(), view);
+
+        String[] args = {"--benchmark", Long.toString(seed)};
+
+        myKamino.main(args);
+
+        assertTrue(myKamino.huntSucceeded());
+        assertTrue(myKamino.gatherSucceeded());
+
+        view.scoreStats();
+        view.timeStats();
+    }
 
 }
